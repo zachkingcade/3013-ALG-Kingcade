1c5fdd2b7fff186b84f7be711bba169cc23d31d5
zachkingcade <zachkingcade@gmail.com>
2020-01-29 08:30:51 -0600

    added comments

--- Assignments/02-CommentedCode/main.cpp
+++ Assignments/02-CommentedCode/main.cpp
@@ -1,3 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   
+// Author:           Zachary Kingcade
+// Email:            zachkingcade@gmail.com
+// Label:            A02
+// Title:            Commenting C++ Code
+// Course:           CMPS 3013
+// Semester:         Spring 2019-2020
+//
+// Description:
+//       This assignment is more about breaking the ice with github, with a little 
+//       to do with programming. Also to set a president when it come to commenting
+//       programs in this class
+//
+/////////////////////////////////////////////////////////////////////////////////
+
 //----------Precompiler Directives----------
 #include <iostream>
 
@@ -6,6 +22,12 @@ using namespace std;
 //----------Global Varibles----------
 int A[100];     //unused int array of size 100
 
+/**
+ * Node
+ * 
+ * Description:
+ *      A Storage wrapper for int varibles that holds pointers to other Nodes
+ */
 struct Node {
     int x;
     Node *next;
@@ -19,11 +41,33 @@ struct Node {
     }
 };
 
+/**
+ * List
+ * 
+ * Description:
+ *      Storage class for int's with access to both front and back pushing 
+ *      and popping
+ * 
+ * Public Methods:
+ *      void        Push(int)
+ *      void        Insert(int)
+ *      void        PrintTail
+ *      string      Print
+ *      int         Pop()
+ *      List        +operator
+ *      int         []operator
+ *      ostream     <<operator
+ * 
+ * Usage: 
+ *      Used to store a list of int values
+ *      
+ */
+
 class List {
 private:
-    Node *Head;
-    Node *Tail;
-    int Size;
+    Node *Head;     //points to the first item in the list
+    Node *Tail;     //points to the last item in the list
+    int Size;       //used to keep track of the number of item in the list
 
 public:
     List() {
@@ -31,6 +75,17 @@ public:
         Size = 0;
     }
 
+    /**
+     * Public: Push
+     * 
+     * Description:
+     *      IF the list is empty it initalizes the Head and Tail and adds an
+     *      item to the list
+     *      ELSE adds an item to the front of the list
+     * 
+     * Params:
+     *      int     val     Value of the item to add to the list
+     */
     void Push(int val) {
         // allocate new memory and init node
         Node *Temp = new Node(val);
@@ -44,6 +99,16 @@ public:
         Size++;
     }
 
+        /**
+     * Public: Insert
+     * 
+     * Description:
+     *      adds an item to the front of the list
+     *      IF the Tail is not initalized, initalizes it
+     * 
+     * Params:
+     *      int     val     Value of the item to add to the list
+     */
     void Insert(int val) {
         // allocate new memory and init node
         Node *Temp = new Node(val);
@@ -57,11 +122,27 @@ public:
         }
         Size++;
     }
-
+    /**
+     * Public:  PrintTail
+     * 
+     * Description:
+     *      Prints the value of the last item of the list
+     */
     void PrintTail() {
         cout << Tail->x << endl;
     }
 
+    /**
+     * Public: Print
+     * 
+     * Description:
+     *      returns a string of all the values of the list 
+     *      seperated by ->'s
+     * 
+     * Returns:
+     *      string of all the values of the list seperated
+     *      by ->'s
+     */
     string Print() {
         Node *Temp = Head;
         string list;
@@ -74,12 +155,35 @@ public:
         return list;
     }
 
+    /**
+     * Public: pop
+     * 
+     * Description:
+     *      removes and return an item from the list
+     * 
+     * Returns:
+     *      return an item from the list
+     */
+
     // not implemented
     int Pop() {
         Size--;
         return 0; //
     }
 
+    /**
+     * Public: + operator
+     * 
+     * Description:
+     *      allows the adding of 2 lists together appending the second
+     *      to the end of the first.
+     * 
+     * Params:
+     *      List    List to be appended
+     * 
+     * Returns:
+     *      A list containing the items of both lists
+     */
     List operator+(const List &Rhs) {
         // Create a new list that will contain both when done
         List NewList;
@@ -106,8 +210,19 @@ public:
         return NewList;
     }
 
-    // Implementation of [] operator.  This function returns an
-    // int value as if the list were an array.
+    /**
+     * Public: Operator []
+     * 
+     * Description:
+     *      Allows access to any item in the list as if it were
+     *      an array
+     * 
+     * Params:
+     *      int     index of what item in the list to access
+     * 
+     * Returns:
+     *      int     value of the item in the list
+     */
     int operator[](int index) {
         Node *Temp = Head;
 
@@ -123,6 +238,20 @@ public:
         }
     }
 
+    /**
+     * Public: Operator []
+     * 
+     * Description:
+     *      Allows the printing of the entire list using
+     *      the << operator
+     * 
+     * Params:
+     *      List        List to be printed
+     * 
+     * Returns:
+     *      ostream     out stream object allowing for the
+     *                  use of multiple << operators in a row
+     */
     friend ostream &operator<<(ostream &os, List L) {
         os << L.Print();
         return os;
