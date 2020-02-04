2c9a4d2b3f44469dafbac7c9b455f1ab0e5cda09
zachkingcade <zachkingcade@gmail.com>
2020-02-04 08:41:23 -0600

    Tested proof of concept

--- Assignments/05-P02/main.cpp
+++ Assignments/05-P02/main.cpp
@@ -1,6 +1,9 @@
 //----------PreCompiler Directives----------
 #include"JsonFacade.hpp"
 #include"mygetch.hpp"
-#include"read_dict.cpp"
 #include"Timer.hpp"
-#include""
+
+//----------Main----------
+int main (){
+    
+}
--- Assignments/05-P02/read_dict.cpp
+++ Assignments/05-P02/read_dict.cpp
@@ -3,8 +3,11 @@
 #include <time.h>
 #include <chrono> 
 #include "Timer.hpp"
+#include <algorithm>
 
 using namespace std;
+
+int theSearch(vector<string> v, string key);
 
 int main(){
     srand(time(NULL));
@@ -22,10 +25,30 @@ int main(){
     string key;                         // key variable to access json object
 
     vector<string> keys = J.getKeys();
-    
-    cout<<keys.size()<<endl;
-    index = rand() % keys.size();
-    key = J.getKey(index);
 
-    cout<<key<<" = "<<J.getValue(key)<<endl;
+    //test of sorting dect
+    sort(keys.begin(),keys.end());
+    
+    // for (int i = 0; i < keys.size(); i++){
+    //     cout << keys[i] << '\n';
+    // }
+    // cout << endl;
+
+    int ndex = theSearch(keys, "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+    string word = ndex >= 0 ? keys[ndex] : "not found.";
+    cout << "Search: " << ndex << "... " << word << "\n";
+
+
+    // cout<<keys.size()<<endl;
+    // index = rand() % keys.size();
+    // key = J.getKey(index);
+
+    // cout<<key<<" = "<<J.getValue(key)<<endl;
+}
+
+
+int theSearch(vector<string> v, string key){
+    int i = lower_bound(v.begin(), v.end(), key) - v.begin();
+    if(v[i] != key) return -1;
+    return i;
 }
